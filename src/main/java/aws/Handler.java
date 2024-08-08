3d73391432a3b2a1d0dd37ac8c2839b47527a771
kropo <kropotovd@gmail.com>
2024-08-08 15:27:15 +0200

    AWS handler and other stuff

---
+++ src/main/java/aws/Handler.java
@@ -0,0 +1,12 @@
+package aws;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class Handler implements RequestHandler<Integer, String> {
+    @Override
+    public String handleRequest(Integer integer, Context context) {
+        System.out.println("handleRequest is called " + context.getFunctionName());
+        return "Hello from handleRequest";
+    }
+}
